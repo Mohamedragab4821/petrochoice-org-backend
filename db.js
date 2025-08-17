457d1e93005a712a7f0c0e4b703f9c79870f90e8
Mohamedragab4821 <103222920+Mohamedragab4821@users.noreply.github.com>
2025-08-17 14:59:47 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '', // use your MySQL password if any
-  database: 'portal_petrochoice', // ✅ your actual DB name
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '', // use your MySQL password if any
+  database: process.env.DB_NAME || 'portal_petrochoice', // ✅ your actual DB name
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
